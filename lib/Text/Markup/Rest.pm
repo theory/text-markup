748791d6639b55cd78e04f9c469d9f7e2cd4c2ea
David E. Wheeler <david@justatheory.com>
2012-01-26 22:16:15 -0800

    Update copyright date.

--- lib/Text/Markup/Rest.pm
+++ lib/Text/Markup/Rest.pm
@@ -114,7 +114,7 @@ Daniele Varrazzo <daniele.varrazzo@gmail.com>
 
 =head1 Copyright and License
 
-Copyright (c) 2011 Daniele Varrazzo. Some Rights Reserved.
+Copyright (c) 2011-2012 Daniele Varrazzo. Some Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
